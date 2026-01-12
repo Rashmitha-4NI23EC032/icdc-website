cdce9b96c5b4446ba2826f9ac48ea289c2f3da4e
Rashmitha-4NI23EC032 <hnrashmitha146@gmail.com>
2026-01-12 12:49:21 +0530

    Create Coordinators component to display faculty

---
+++ src/components/Coordinators.jsx
@@ -0,0 +1,11 @@
+function Coordinators() {
+  return (
+    <section id="coordinators" className="section">
+      <h2>Faculty Coordinators</h2>
+      <p>Dr. Remya Jayachandran</p>
+      <p>Dr. Ashok K</p>
+    </section>
+  );
+}
+
+export default Coordinators;
