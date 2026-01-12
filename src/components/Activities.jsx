3a823da95edaa7dbbcfad4e8ce3ee82de5f922b1
Rashmitha-4NI23EC032 <hnrashmitha146@gmail.com>
2026-01-12 12:48:49 +0530

    Add Activities component with event list

---
+++ src/components/Activities.jsx
@@ -0,0 +1,15 @@
+function Activities() {
+  return (
+    <section id="activities" className="section light">
+      <h2>Activities</h2>
+      <ul>
+        <li>IC Design Workshops</li>
+        <li>Project Expo & Hackathons</li>
+        <li>Industry Expert Talks</li>
+        <li>Hands-on VLSI Training</li>
+      </ul>
+    </section>
+  );
+}
+
+export default Activities;
