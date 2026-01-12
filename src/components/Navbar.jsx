dd6d611f05ccf101957c230a145d3955e2b83928
Rashmitha-4NI23EC032 <hnrashmitha146@gmail.com>
2026-01-12 12:46:12 +0530

    Create Navbar.jsx

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <h2>ICDC</h2>
+      <ul>
+        <li><a href="#about">About</a></li>
+        <li><a href="#activities">Activities</a></li>
+        <li><a href="#coordinators">Coordinators</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
