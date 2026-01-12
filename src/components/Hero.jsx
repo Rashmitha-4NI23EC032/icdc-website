c1862d3a71d494dd76cd3e097fa4d580e4e3a46d
Rashmitha-4NI23EC032 <hnrashmitha146@gmail.com>
2026-01-12 12:47:00 +0530

    Create Hero.jsx

---
+++ src/components/Hero.jsx
@@ -0,0 +1,10 @@
+function Hero() {
+  return (
+    <section className="hero">
+      <h1>Integrated Circuits Design Club</h1>
+      <p>The National Institute of Engineering, Mysuru</p>
+    </section>
+  );
+}
+
+export default Hero;
