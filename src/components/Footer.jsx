c16077218e958ecafc52907854a4956051ae1788
Rashmitha-4NI23EC032 <hnrashmitha146@gmail.com>
2026-01-12 12:50:05 +0530

    Add Footer component with club information

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+function Footer() {
+  return (
+    <footer id="contact" className="footer">
+      <p>Integrated Circuits Design Club (ICDC)</p>
+      <p>The National Institute of Engineering, Mysuru</p>
+      <p>© 2026 ICDC</p>
+    </footer>
+  );
+}
+
+export default Footer;
